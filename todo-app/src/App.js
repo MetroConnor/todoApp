0d9b2c829d5096fcbd175a179fb890c112bcf9a0
Connor <connorzupan@googlemail.com>
2024-06-03 12:35:37 +0200

    added Frontend and Create/Change/Delete functions

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,29 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
-  React.useEffect(() => {
-      createNavigationBar();
-  }, []);
+    const [todos, setTodos] = useState([
+        { id: 1, text: 'Test 1' },
+        { id: 2, text: 'Test 2' }
+    ]);
 
-  return (
-    <div className="App" id="navbar">
-      <h1>My ToDo List!</h1>
-    </div>
-  );
+    const addTodo = () => {
+        const newTodo = { id: Date.now(), text: 'Todo eintragen' };
+        setTodos([...todos, newTodo]);
+    };
+
+    const changeTodo = (id) => {
+        const newText = prompt('Text eingeben: ');
+        setTodos(todos.map(todo => todo.id === id ? { ...todo, text: newText } : todo));
+    };
+
+    const deleteTodo = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
+    return (
+        <div className="App">
+            <div className="navbar">
+                <h1>My ToDo List!</h1>
+                <button onClick={addTodo}>Create</button>
+            </div>
+            <div className="todo-list">
+                {todos.map(todo => (
+                    <div key={todo.id} className="todo-item">
+                        <span className="todo-text">{todo.text}</span>
+                        <input type="checkbox" className="todo-checkbox" />
+                        <div className="todo-buttons">
+                            <button onClick={() => changeTodo(todo.id)}>Change</button>
+                            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
-function createNavigationBar(){
-    const navbar = document.getElementById('navbar');
-    const buttonCreate = document.createElement('button');
-    buttonCreate.textContent = 'Create';
-    const buttonChange = document.createElement('button');
-    buttonChange.textContent = 'Change';
-    const buttonDelete = document.createElement('button');
-    buttonDelete.textContent = 'Delete';
-
-    navbar.appendChild(buttonCreate);
-    navbar.appendChild(buttonChange);
-    navbar.appendChild(buttonDelete);
-}
 export default App;
